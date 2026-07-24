ac83f496c0c1039a1df65e55fbd7624695792b71
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-07-24 03:45:05 -0300

    feat(mbr): Master Boot Record entry struct

---
+++ include/mbr.h
@@ -0,0 +1,25 @@
+/**
+ * @file mbr.h
+ * @author Loren Montilla
+ *
+ * @license SPDX-License-Identifier: GPL-3.0+ (see COPYING)
+ **/
+#pragma once
+
+#include <stdint.h>
+
+#pragma pack(push, 1)
+
+/**
+ * Master Boot Record Entry
+ * */
+typedef struct {
+  uint8_t status;
+  uint8_t chs_start[3];
+  uint8_t type;
+  uint8_t chs_end[3];
+  uint32_t lba_start;
+  uint32_t num_sectors;
+
+} MBR_Entry;
+#pragma pack(pop)
