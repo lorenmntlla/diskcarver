44993207d0264449cac6bf4c7bc1795af6f9e7c7
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-07-27 00:11:07 -0300

    feat(partition_table): determines if partition table is GUID

--- src/partition_table.c
+++ src/partition_table.c
@@ -1,15 +1,32 @@
 #include "../include/partition_table.h"
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 PartitionTable get_PartitionTable(Disk *disk) {
-  uint8_t header[512];
+  uint8_t *header = calloc(1, disk->sector_size);
 
-  disk_read(disk, header, NO_ADVANCE);
+  disk_read(disk, header);
 
-  if (header[510] == 0x55 && header[511] == 0xAAll)
-    return MASTER_BOOT_RECORD;
+  if (header[0x1FE] != 0x55 || header[0x1FF] != 0xAA) {
+    free(header);
+    disk_seek(disk, 0, SEEK_SET);
 
-  //TODO: check for GUID Partition Table
+    return INVALID;
+  }
 
-  return INVALID;
+  if (disk->sector_size < 1024)
+    disk_read(disk, header);
+
+  disk_seek(disk, 0, SEEK_SET);
+
+  if (strncmp((char *)header, "EFI PART", 8) == 0) {
+    free(header);
+
+    return GUID;
+  }
+
+  free(header);
+
+  return MASTER_BOOT_RECORD;
 }
