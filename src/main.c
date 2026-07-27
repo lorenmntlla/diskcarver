f43ea5426fdb9d7a810e5dbf95e24954d769bc52
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-07-27 00:13:12 -0300

    chore(main): print MBR partition entries from path

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include "../include/disk.h"
 #include "../include/mbr.h"
 #include "../include/partition_table.h"
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,16 +17,30 @@ int main(int argc, char **argv) {
   if (!disk)
     return EXIT_FAILURE;
 
-  if (get_PartitionTable(disk) == MASTER_BOOT_RECORD) {
-    MBR_Entry *partitions = get_MBR_Entries(disk);
+  switch (get_PartitionTable(disk)) {
+  case INVALID: {
+    puts("Disk does not contain a supported partition table");
+    break;
+  }
+  case MASTER_BOOT_RECORD: {
+    puts("Partition table is Master Boot Record");
+    MBR_Entry *part = get_MBR_entries(disk);
 
     for (size_t i = 0; i < 4; i++) {
-      printf("partition %lu status: %#x\n", i, partitions[i].status);
+      printf("partition %lu:\n", i + 1);
+      printf("\tstatus: %#2.2x\n", part[i].status);
+      printf("\ttype: %#2.2x\n", part[i].type);
+      printf("\tLBA of first sector: %u\n", part[i].lba_start);
+      printf("\tNumber of sectors: %u\n", part[i].num_sectors);
     }
 
-    free(partitions);
-  } else
-    printf("Partition table is not MBR\n");
+    free(part);
+    break;
+  }
+  case GUID: {
+    puts("Partition table is GUID");
+  } break;
+  }
 
   disk_close(disk);
 
