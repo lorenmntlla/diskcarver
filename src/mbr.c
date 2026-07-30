901c87e97b74ddb5aec6b5e49216b93461e479c1
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-07-29 21:48:59 -0300

    fix(mbr/partiton_table): place disk offset in the same position before being called instead of setting it to zero, possibly disrupting current_sector

--- src/mbr.c
+++ src/mbr.c
@@ -9,19 +9,22 @@ MBR_Entry *get_MBR_entries(Disk *disk) {
 
   uint8_t *header = calloc(1, disk->sector_size);
 
+  off_t previous_offset = disk_seek(disk, 0, SEEK_CUR);
+
   disk_seek(disk, 0, SEEK_SET);
   ssize_t read = disk_read(disk, header);
 
   if (read < 512) {
     free(entries);
+    disk_seek(disk, previous_offset, SEEK_SET);
+
     return NULL;
   }
 
   memcpy(entries, header + 0x01BE, 64);
 
   free(header);
-
-  disk_seek(disk, 0, SEEK_SET);
+  disk_seek(disk, previous_offset, SEEK_SET);
 
   return entries;
 }
--- src/partition_table.c
+++ src/partition_table.c
@@ -6,12 +6,14 @@
 PartitionTable get_PartitionTable(Disk *disk) {
   uint8_t *header = calloc(1, disk->sector_size);
 
+  off_t previous_offset = disk_seek(disk, 0, SEEK_CUR);
+
   disk_seek(disk, 0, SEEK_SET);
   disk_read(disk, header);
 
   if (header[0x1FE] != 0x55 || header[0x1FF] != 0xAA) {
     free(header);
-    disk_seek(disk, 0, SEEK_SET);
+    disk_seek(disk, previous_offset, SEEK_SET);
 
     return INVALID;
   }
@@ -21,13 +23,13 @@ PartitionTable get_PartitionTable(Disk *disk) {
 
   if (strncmp((char *)header, "EFI PART", 8) == 0) {
     free(header);
-    disk_seek(disk, 0, SEEK_SET);
+    disk_seek(disk, previous_offset, SEEK_SET);
 
     return GUID;
   }
 
   free(header);
-  disk_seek(disk, 0, SEEK_SET);
+  disk_seek(disk, previous_offset, SEEK_SET);
 
   return MASTER_BOOT_RECORD;
 }
